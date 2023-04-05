d92edb94b0a1903dc5fdf565c4a0ab2baa8e1344
litao <2397933486@qq.com>
2023-04-05 22:36:56 +0800

    2023年4月5日 mybatis完结

---
+++ mybatis_study/mybatis-005/src/main/java/com/powernode/mybatis/Util/SqlSessionUtil.java
@@ -0,0 +1,42 @@
+package com.powernode.mybatis.Util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+
+public class SqlSessionUtil {
+
+    private static SqlSessionFactory sqlSessionFactory =null;
+    private SqlSessionUtil(){
+
+    }
+    static {
+
+        try {
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            sqlSessionFactory = sqlSessionFactoryBuilder.build(Resources.getResourceAsStream("mybatis-config.xml"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    private  static  ThreadLocal<SqlSession> local = new ThreadLocal<>();
+    public static SqlSession openSession(){
+        SqlSession sqlSession = local.get();
+        if(sqlSession==null){
+            sqlSession =sqlSessionFactory.openSession();
+            //将sql对象绑定到当前线程
+            local.set(sqlSession);
+        }
+        return sqlSession;
+    }
+    public static void close(SqlSession sqlSession){
+        if(sqlSession!=null){
+            sqlSession.close();
+            local.remove();
+        }
+    }
+}
